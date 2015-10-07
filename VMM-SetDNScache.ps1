ac5318c03f18a512d4463ca413b0bb211935148a
vniklas <niklas@djungeln.se>
2015-10-07 14:04:20 +0200

    added vmm dns cache commands

---
+++ VMM-SetDNScache.ps1
@@ -0,0 +1,7 @@
+﻿# Configure TTL for VMM server to success in BareMetal deploy
+
+New-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Dnscache\Parameters" -Name "MaxCacheTtl" -Value 5 -PropertyType "DWORD" -Force
+
+New-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Dnscache\Parameters" -Name "MaxNegativeCacheTtl" -Value 5 -PropertyType "DWORD" -Force
+
+Restart-Service -Name Dnscache -Force –Verbose
